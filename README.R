74488f2dd35d2761632b0c41240911e09a37043f
Alex Axthelm <aaxthelm@RMI.org>
2024-05-18 22:42:05 +0200

    Initial Commit

---
+++ README.R
@@ -0,0 +1,14 @@
+# nonogramr
+
+R package for importing, solving and vizualizing nonograms.
+
+## TODO
+
+- [ ] Importing
+  - [ ] `.non` files
+- [ ] Vizualizing
+  - [ ] Plotting
+  - [ ] Shiny App?
+- [ ] Solving
+  - [ ] Line Solving
+- [ ] Tracking Game State
