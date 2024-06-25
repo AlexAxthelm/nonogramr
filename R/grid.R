5ca4eebe8f2488f556c451450590d7515ed50e1b
Alex Axthelm <aaxthelm@RMI.org>
2024-06-25 19:14:01 +0200

    Add `plot` method for `grid`

--- R/grid.R
+++ R/grid.R
@@ -15,3 +15,28 @@ grid <- S7::new_class("grid",
     new_object(S7_object, width = width, height = height, cells = cells)
   }
 )
+
+plot <- S7::new_generic("plot", "grid")
+S7::method(plot, grid) <- function(grid) {
+  ggplot2::ggplot(
+    data = grid@cells,
+    mapping = ggplot2::aes(
+      x = x,
+      y = y,
+      fill = color
+    )
+  ) +
+    ggplot2::geom_tile(width = 1L, height = 1L) +
+    ggplot2::scale_fill_gradient(low = "white", high = "black") +
+    ggplot2::theme_void() +
+    ggplot2::coord_fixed() +
+    ggplot2::geom_vline(
+      xintercept = seq(0.5, grid@width + 0.5, by = 1L),
+      color = "grey75"
+    ) +
+    ggplot2::geom_hline(
+      yintercept = seq(0.5, grid@height + 0.5, by = 1L),
+      color = "grey75"
+    ) +
+    ggplot2::theme(legend.position = "none")
+}
