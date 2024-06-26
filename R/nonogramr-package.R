5552cf3fe1c63c30d3e28d592c1db0f6f03da1ba
Alex Axthelm <aaxthelm@RMI.org>
2024-06-26 22:47:49 +0200

    Allow use with R < 4.3

--- R/nonogramr-package.R
+++ R/nonogramr-package.R
@@ -10,3 +10,7 @@
 #' @importFrom rlang .data
 ## usethis namespace: end
 NULL
+
+# enable usage of <S7_object>@name in package code
+#' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
+NULL
