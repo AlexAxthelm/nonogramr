de9d67ea8d98f7abcb3f89dbe7ca18d8d6c886ec
Alex Axthelm <aaxthelm@RMI.org>
2024-06-29 23:51:01 +0200

    Update documentation

--- R/datasets.R
+++ R/datasets.R
@@ -2,13 +2,18 @@
 #' @include puzzle.R
 
 #' Demo puzzle
+#' @name demo_puzzle
+#' @format a puzzle S7 object
 #'
 #' A simple sample puzzle to show how the package works.
 #' Hint: it's a glider from Conway's Game of Life.
 "demo_puzzle"
 
 #' Hi puzzle
+#' @rdname demo_puzzle
+#' @format a puzzle S7 object
 #'
 #' A simple  asymmetric sample puzzle to show how the package works.
 #' Hint: it's block text Hi! in a minimal piizel font
 "hi_puzzle"
+
