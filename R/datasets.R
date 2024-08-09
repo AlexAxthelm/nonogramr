d07df4eaea97cbd2ff71db1f074edcdc9d45face
Alex Axthelm <aaxthelm@RMI.org>
2024-08-09 11:40:45 +0200

    Document Datasets

--- R/datasets.R
+++ R/datasets.R
@@ -1,10 +1,22 @@
 # include for collation
 #' @include puzzle.R
 
+#' Demo puzzle
+#'
+#' A simple sample puzzle to show how the package works.
+#' Hint: it's a glider from Conway's Game of Life.
+#'
+#' @name demo_puzzle
+#' @title Demo puzzle
+#' @format a puzzle S7 object. See \code{puzzle} for details.
+"demo_puzzle"
+
 #' Hi puzzle
-#' @rdname demo_puzzle
-#' @format a puzzle S7 object
 #'
 #' A simple  asymmetric sample puzzle to show how the package works.
 #' Hint: it's block text Hi! in a minimal piizel font
+#'
+#' @name hi_puzzle
+#' @title Hi puzzle
+#' @format a puzzle S7 object. See \code{puzzle} for details.
 "hi_puzzle"
